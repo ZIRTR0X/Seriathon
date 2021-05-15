fd756a5d9c4147dc0c2a28e83092d08a4ee2cbd0
ZIRTR0X <jujuping66@gmail.com>
2021-05-15 13:39:04 +0000

    Ajout de Utilisateur.cs
    git-svn-id: https://forge.clermont-universite.fr/svn/md-binome02@35 a295b7a2-e4ef-4650-9f47-075ab073bfd3

---
+++ trunc/src/Sérieathon/modelisation/Utilisateur.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace modelisation
+{
+    class Utilisateur
+    {
+        string pseudo;
+        string password;
+        string email;
+        int age;
+        string genre;
+        LinkedList<ContenuVideoludique> ListCV = new LinkedList<ContenuVideoludique>;
+
+        public Utilisateur(string pseudo, string password, string email, int age, string genre) 
+        {
+            this.pseudo = pseudo;
+            this.password = password;
+            this.email = email;
+            this.age = age;
+            this.genre = genre;
+        }
+
+        public void ajouterCommeVu(ContenuVideoludique c)
+        {
+            ListCV.AddLast(c);
+        }
+
+        public void enleverCommeVu(ContenuVideoludique c)
+        {
+            ListCV.Remove(c);
+        }
+
+        public int getNbAnimeVu()
+        {
+            int nbAnime = 0;
+            foreach (ContenuVideoludique a in ListCV) {
+                if (a.GetType() == "Anime")
+                {
+                    nbAnime++;
+                }
+            }
+            return nbAnime;
+        }
+
+        public int getNbSerieVu()
+        {
+            int nbSerie = 0;
+            foreach (ContenuVideoludique a in ListCV)
+            {
+                if (a.GetType() == "Serie")
+                {
+                    nbSerie++;
+                }
+            }
+            return nbSerie;
+        }
+
+        public int getNbFilmVu()
+        {
+            int nbFilm = 0;
+            foreach (ContenuVideoludique a in ListCV)
+            {
+                if (a.GetType() == "Film")
+                {
+                    nbFilm++;
+                }
+            }
+            return nbFilm;
+        }
+
+        public int getNbContenuVu()
+        {
+            return ListCV.Count;
+        }
+
+        public int getNbGenreGlobalVu(GenreGlobal g)
+        {
+            int nbGenreGlobal = 0;
+
+            foreach (ContenuVideoludique a in ListCV)
+            {
+                if (a.Genre == g)
+                {
+                    nbGenreGlobal++;
+                }
+            }
+
+            return nbGenreGlobal;
+        }
+
+        public int getNbGenreAnimeVu(GenreAnime a)
+        {
+            int nbGenreAnime = 0;
+
+            foreach (ContenuVideoludique b in ListCV)
+            {
+                if (b.Genre == a)
+                {
+                    nbGenreAnime++;
+                }
+            }
+
+            return nbGenreAnime;
+        }
+    }
+}
