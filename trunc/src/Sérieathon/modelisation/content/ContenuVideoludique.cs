8bfff3f122b7a192c175284ca36ccb28309a22aa
ZIRTR0X <jujuping66@gmail.com>
2021-05-18 06:10:20 +0000

    modification ContenuVideoludique.cs
    git-svn-id: https://forge.clermont-universite.fr/svn/md-binome02@40 a295b7a2-e4ef-4650-9f47-075ab073bfd3

--- trunc/src/Sérieathon/modelisation/content/ContenuVideoludique.cs
+++ trunc/src/Sérieathon/modelisation/content/ContenuVideoludique.cs
@@ -1,17 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using modelisation.genres;
 
 namespace modelisation.content
 {
     public abstract class ContenuVideoludique
     {
-        public string Titre { get; set; }
+        private string Titre { get; set; }
+        private TimeSpan Duree { get; set; }
+        private bool Vue { get; set; } = false;
+        private string Production { get; set; }
+        private int Date { get; set; }
+        public GenreGlobal Genre { get; set; }
+        private string Description { get; set; }
+        private string OuRegarder { get; set; }
+        private string Audio { get; set; }
+        private string SousTitre { get; set; }
 
-        public TimeSpan Duree { get; set; }
 
-        public bool Like { get; set; } = false;
+        public ContenuVideoludique(string Titre, TimeSpan Duree,string Production, int Date, GenreGlobal Genre, string Description, string OuRegarder, string Audio, string SousTitre)
+        {
+            this.Titre = Titre;
+            this.Duree = Duree;
+            this.Production = Production;
+            this.Date = Date;
+            this.Genre = Genre;
+            this.Description = Description;
+            this.OuRegarder = OuRegarder;
+            this.Audio = Audio;
+            this.SousTitre = SousTitre;
+        }
 
-        public string StudioProd { get; set; }
+
+       
+
     }
 }
